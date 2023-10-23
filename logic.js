f09b7cede018beb7b8953e50ffcbadab4e9895e2
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-10-23 06:25:28 +0000

    Beginning creation of dialog form input

--- logic.js
+++ logic.js
@@ -45,45 +45,48 @@ myLibrary.push(book2);
 // console.log(myLibrary[1].title); -- this allows access of the books stuff
 
 
-const add_book = document.querySelector(".add-book button");
-const cards = document.querySelector(".cards");
 
+
+// const add_book = document.querySelector(".add-book button");
+// const cards = document.querySelector(".cards");
 
 // Create a dummy function to add the card, and all elements to the pages in preparation for doing so
 // with a form and class
-add_book.addEventListener("click", function (e) {
-    // console.log(e);
-    const card = document.createElement("div");
-    const title = document.createElement("div");
-    const author = document.createElement("div");
-    const pages = document.createElement("div");
-    const released = document.createElement("div");
-    const read = document.createElement("button");
-    const remove = document.createElement("button");
 
-    // Assign classes to all elements
-    card.className = "card";
-    title.className = "title";
-    author.className = "author";
-    pages.className = "pages";
-    released.className = "released";
-    read.className = "read";
+// add_book.addEventListener("click", function (e) {
+//     // console.log(e);
+//     const card = document.createElement("div");
+//     const title = document.createElement("div");
+//     const author = document.createElement("div");
+//     const pages = document.createElement("div");
+//     const released = document.createElement("div");
+//     const read = document.createElement("button");
+//     const remove = document.createElement("button");
 
-    // Assign innerHTML content
-    title.innerHTML = "The Stone Door"
-    author.innerHTML = "Patrick Rothfuss";
-    pages.innerHTML = "1120";
-    pages.innerHTML = `${pages.innerHTML} pages`;
-    released.innerHTML = "01 March 2016";
-    read.innerHTML = "Not Read";
-    remove.innerHTML = "Remove book";
+//     // Assign classes to all elements
+//     card.className = "card";
+//     title.className = "title";
+//     author.className = "author";
+//     pages.className = "pages";
+//     released.className = "released";
+//     read.className = "read";
 
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(released);
-    card.appendChild(read);
-    card.appendChild(remove);
+//     // Assign innerHTML content
+//     title.innerHTML = "The Stone Door"
+//     author.innerHTML = "Patrick Rothfuss";
+//     pages.innerHTML = "1120";
+//     pages.innerHTML = `${pages.innerHTML} pages`;
+//     released.innerHTML = "01 March 2016";
+//     read.innerHTML = "Not Read";
+//     remove.innerHTML = "Remove book";
 
-    cards.appendChild(card);
-})
+//     // Add elements to the card
+//     card.appendChild(title);
+//     card.appendChild(author);
+//     card.appendChild(pages);
+//     card.appendChild(released);
+//     card.appendChild(read);
+//     card.appendChild(remove);
+
+//     cards.appendChild(card);
+// })
