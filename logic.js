13e8bd9c53aa56129ab496e6883991eadf51ce8b
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-10-24 06:08:54 +0000

    Changes CSS var colors, changed default card
    Changed the appearance of the default card -- by default, has a lighter
    color scheme, when the book has been read, the color scheme will invert
    and get darker. I also added a removable and addable class that can be
    toggled later on using CSS. The primary, secondary, and tertiary colors
    for the webpage were modified as well.

--- logic.js
+++ logic.js
@@ -8,24 +8,17 @@ function Book(title, author, pages, date_released, read) {
     this.pages = pages;
     this.date_released = date_released;
     this.read = read;
-
-    // this.info = function () {
-    //     if (this.read) {
-    //         str = `${title} by ${author}, ${pages} pages, has been read`;
-    //     }
-    //     else {
-    //         str = `${title} by ${author}, ${pages} pages, not read yet`;
-    //     }
-    //     return str;
-    // }
 }
+
+
 const open_dialog = document.querySelector(".add-book button");
 const cards = document.querySelector(".cards");
 const closeButton = document.querySelector("dialog button");
 const dialog = document.querySelector("dialog");
 
+
 open_dialog.addEventListener("click", openDialog);
-closeButton.addEventListener("click", saveInput);
+closeButton.addEventListener("click", addBookToLibrary);
 
 function openDialog() {
     dialog.style.display = "contents";
@@ -37,59 +30,64 @@ function addBookToLibrary(event) {
     dialog.close();
     dialog.style.display = "none";
 
+    // Save form information
     const title_in = document.querySelector("#title");
     const author_in = document.querySelector("#author");
     const pages_in = document.querySelector("#pages");
     const date_in = document.querySelector("#date");
     const read_in = document.querySelector("#read");
 
-    /* How to return the value of the input */
-    console.log(title_in.value);
-    console.log(author_in.value);
-    console.log(pages_in.value);
-    console.log(date_in.value);
-    console.log(read_in.value);
+    // Create a book using form information
+    book1 = new Book(title_in.value, author_in.value, pages_in.value, date_in.value, read_in.value);
+    myLibrary.push(book1);
+    console.log(myLibrary[0]);
 
-    // book1 = new Book(title_in.value, author_in.value, pages_in.value, date_in.value, read_in.value);
-    // myLibrary.push(book1);
-    // console.log(myLibrary[0].title);
-    // title_out.innerHTML = `${title_in.value}`
-    // console.log(`${title_out.innerHTML} Is the title_out innerhtml`);
     document.querySelector("form").reset();
+
+    createCard();
 }
 
+function createCard() {
+
+    const card = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const date = document.createElement("div");
+    const read = document.createElement("button");
+    const remove = document.createElement("button");
+
+    card.className = "card";
+    title.className = "title";
+    author.className = "author";
+    pages.className = "pages";
+    date.className = "released";
+    read.className = "read";
+
+    title.innerHTML = `${myLibrary[0].title}`;
+    author.innerHTML = `${myLibrary[0].author}`;
+    pages.innerHTML = `${myLibrary[0].pages}`;
+    date.innerHTML = `${myLibrary[0].date}`;
+    remove.innerHTML = "Remove Book";
+
+    // TODO: Create a small section that will add or remove a class to style
+    // the card and elements based on the value of read or not.
+    read.innerHTML = "Not Read";  /* `${myLibrary[0].read}`; */
 
 
-// const remove = document.createElement("button");
-// const card = document.createElement("div");
-// const title_out = document.createElement("div");
-// const author_out = document.createElement("div");
-// const pages_out = document.createElement("div");
-// const released_out = document.createElement("div");
-// const read_out = document.createElement("button");
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(date);
+    card.appendChild(read);
+    card.appendChild(remove);
 
-// card.className = "card";
-// title_out.className = "title";
-// author_out.className = "author";
-// pages_out.className = "pages";
-// released_out.className = "released";
-// read_out.className = "read";
+    cards.appendChild(card);
+}
 
-// author.innerHTML = "Patrick Rothfuss";
-// pages.innerHTML = "1120";
-// pages.innerHTML = `${pages.innerHTML} pages`;
-// released.innerHTML = "01 March 2016";
-// read.innerHTML = "Not Read";
-// remove.innerHTML = "Remove book";
+function removeBook() {
 
-// // Add elements to the card
-// card.appendChild(title);
-// card.appendChild(author);
-// card.appendChild(pages);
-// card.appendChild(released);
-// card.appendChild(read);
-// card.appendChild(remove);
-
+}
 
 
 
